a18bdc29db360bcdfc50836142d7f2698a1c63cc
Tientjie <tientjieit@gmail.com>
2024-04-17 12:32:41 +0200

    test-project

---
+++ Backend/portal-api/build.gradle.kts
@@ -0,0 +1,79 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+  java
+  application
+  id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "io.epos"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+val vertxVersion = "4.5.7"
+val junitJupiterVersion = "5.9.1"
+
+val mainVerticleName = "io.epos.portal_api.verticle.MainVerticle"
+val launcherClassName = "io.vertx.core.Launcher"
+
+val watchForChange = "src/**/*"
+val doOnChange = "${projectDir}/gradlew classes"
+
+application {
+  mainClass.set(launcherClassName)
+}
+
+dependencies {
+  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+  implementation("io.vertx:vertx-web-client")
+  implementation("io.vertx:vertx-web-validation")
+  implementation("io.vertx:vertx-sql-client-templates")
+  implementation("io.vertx:vertx-web")
+  implementation("io.vertx:vertx-pg-client")
+  implementation("io.vertx:vertx-json-schema")
+  // https://mvnrepository.com/artifact/io.vertx/vertx-health-check
+  implementation("io.vertx:vertx-health-check:4.5.7")
+  // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+// https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
+  implementation("com.fasterxml.jackson.core:jackson-core:2.17.0")
+// https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations
+  implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.0")
+  implementation("com.ongres.scram:client:2.1")
+  // https://mvnrepository.com/artifact/org.flywaydb/flyway-core
+  implementation("org.flywaydb:flyway-core:10.11.0")
+// https://mvnrepository.com/artifact/org.postgresql/postgresql
+  implementation("org.postgresql:postgresql:42.7.3")
+// https://mvnrepository.com/artifact/org.flywaydb/flyway-database-postgresql
+  runtimeOnly("org.flywaydb:flyway-database-postgresql:10.11.0")
+  testImplementation("io.vertx:vertx-junit5")
+  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<ShadowJar> {
+  archiveClassifier.set("fat")
+  manifest {
+    attributes(mapOf("Main-Verticle" to mainVerticleName))
+  }
+  mergeServiceFiles()
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+tasks.withType<JavaExec> {
+  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+}
---
+++ Backend/portal-api/src/main/java/io/epos/portal_api/verticle/ApiVerticle.java
@@ -0,0 +1,61 @@
+package io.epos.portal_api.verticle;
+
+import io.epos.portal_api.api.handler.BookHandler;
+import io.epos.portal_api.api.handler.BookValidationHandler;
+import io.epos.portal_api.api.repository.BookRepository;
+import io.epos.portal_api.api.router.BookRouter;
+import io.epos.portal_api.api.router.HealthCheckRouter;
+import io.epos.portal_api.api.service.BookService;
+import io.epos.portal_api.util.DbUtils;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.pgclient.PgPool;
+
+public class ApiVerticle extends AbstractVerticle {
+
+
+  @Override
+  public void start(Promise<Void> promise) {
+    final PgPool dbClient = DbUtils.buildDbClient(vertx);
+
+    final BookRepository bookRepository = new BookRepository();
+    final BookService bookService = new BookService(dbClient, bookRepository);
+    final BookHandler bookHandler = new BookHandler(bookService);
+    final BookValidationHandler bookValidationHandler = new BookValidationHandler(vertx);
+    final BookRouter bookRouter = new BookRouter(vertx, bookHandler, bookValidationHandler);
+
+    final Router router = Router.router(vertx);
+    // test endpoint
+    router.get("/").handler(ctx -> ctx.response().end("Hello World!"));
+//    ErrorHandler.buildHandler(router);
+    HealthCheckRouter.setRouter(vertx, router, dbClient);
+    bookRouter.setRouter(router);
+
+    buildHttpServer(vertx, promise, router);
+  }
+
+  /**
+   * Run HTTP server on port 8888 with specified routes
+   *
+   * @param vertx   Vertx context
+   * @param promise Callback
+   * @param router  Router
+   */
+  private void buildHttpServer(Vertx vertx,
+                               Promise<Void> promise,
+                               Router router) {
+    final int port = 8888;
+
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(port, http -> {
+        if (http.succeeded()) {
+          promise.complete();
+        } else {
+          promise.fail(http.cause());
+        }
+      });
+  }
+}
