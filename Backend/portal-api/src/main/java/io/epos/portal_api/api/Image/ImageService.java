87a8fc32e1b215a44835a4e337f6774d50f2ce1d
Tientjie <tientjieit@gmail.com>
2024-04-24 23:09:36 +0200

    delete backend folder

--- Backend/portal-api/src/main/java/io/epos/portal_api/api/Image/ImageService.java
+++
@@ -1,9 +0,0 @@
-package io.epos.portal_api.api.Image;
-
-import io.vertx.sqlclient.Pool;
-
-public class ImageService {
-  public ImageService(Pool dbClient, ImageRepository imageRepository) {
-
-  }
-}
