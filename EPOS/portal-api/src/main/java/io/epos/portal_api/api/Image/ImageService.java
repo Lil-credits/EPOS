c5a9a31f8e4a1dbbe850407de9f633c428b9f5ca
Tientjie <tientjieit@gmail.com>
2024-04-24 23:05:22 +0200

    fix loading schema json and added images endpoint

---
+++ EPOS/portal-api/src/main/java/io/epos/portal_api/api/Image/ImageService.java
@@ -0,0 +1,9 @@
+package io.epos.portal_api.api.Image;
+
+import io.vertx.sqlclient.Pool;
+
+public class ImageService {
+  public ImageService(Pool dbClient, ImageRepository imageRepository) {
+
+  }
+}
