ecc7f350ea961fd808ba19bae0cd6f0d84fc7d3e
echo "Jasper <jasper@veraart.cloud>
2024-05-14 11:01:05 +0200

    Update V1__initial_schema.sql
    Updated SQL script to new needs

--- EPOS/portal-api/src/main/resources/db/migration/V1__initial_schema.sql
+++ EPOS/portal-api/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,7 +1,19 @@
+CREATE TABLE IF NOT EXISTS Organisation (
+  Id SERIAL PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS TeamFaculty (
+  Id SERIAL PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  FOREIGN KEY (OrgansationId) REFERENCES Organisation(Id)
+);
+
 -- Team Table
 CREATE TABLE IF NOT EXISTS Teams (
   Id SERIAL PRIMARY KEY,
-  Name VARCHAR(255) NOT NULL
+  Name VARCHAR(255) NOT NULL,
+  FOREIGN KEY (FacultyId) REFERENCES TeamFaculty(Id)
 );
 
 -- EducationModule Table
@@ -13,22 +25,13 @@ CREATE TABLE IF NOT EXISTS EducationModule (
   FOREIGN KEY (TeamID) REFERENCES Teams(Id)
 );
 
--- TeamRelation Table
-CREATE TABLE IF NOT EXISTS TeamRelation (
-  ParentTeamId INT NOT NULL,
-  ChildTeamId INT NOT NULL,
-  PRIMARY KEY (ParentTeamId, ChildTeamId),
-  FOREIGN KEY (ParentTeamId) REFERENCES Teams(Id),
-  FOREIGN KEY (ChildTeamId) REFERENCES Teams(Id)
-);
-
-
-
 
 -- User Table
 CREATE TABLE IF NOT EXISTS Users (
   Id SERIAL PRIMARY KEY,
   Name VARCHAR(255) NOT NULL
+  DiD VARCHAR(1024) NOT NULL
+  Jwk VARCHAR(2048) NOT NULL
 );
 
 -- TeamUser Table
@@ -60,6 +63,8 @@ CREATE TABLE IF NOT EXISTS IssuedMicroCredential (
   Receiver INT,
   Status VARCHAR(50),
   EducationModuleVersionID INT,
+  Credential JSONB,
+  IssuedTime TIMESTAMP;
   FOREIGN KEY (Issuer) REFERENCES Users(Id),
   FOREIGN KEY (Receiver) REFERENCES Users(Id),
   FOREIGN KEY (EducationModuleVersionID) REFERENCES EducationModuleVersion(Id)
