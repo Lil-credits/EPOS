802ecfccff824e8da23b00042828e61a921e728e
Tientjie <tientjieit@gmail.com>
2024-05-15 11:15:28 +0200

    populate database script

--- EPOS/portal-api/intialise_database/initialise_database.py
+++ EPOS/portal-api/intialise_database/initialise_database.py
@@ -1,8 +1,9 @@
 import psycopg2
 import requests
+import json
+from psycopg2 import Error
 
 """Database INFO"""
-
 USER = "postgres"
 PASSWORD = "AXEe263eqPFqwy4z"
 HOST = "localhost"
@@ -13,12 +14,19 @@ ORGANISATION = "Hogeschool Utrecht"
 TEAMFACULTY = "Natuur & Techniek"
 TEAM = "Open-ICT"
 COURSE = "Open Innovation Semester 2 2023-2024 Backend"
+MODULE_VERSION_DATA = {
+    "Version": 1,
+    "Description": "First version",
+    "Attributes": {"attribute1": "value1", "attribute2": "value2"},
+    "RequiredAchievements": {
+        "achievement1": "value1",
+        "achievement2": "value2",
+    },
+    "Skills": {"skill1": "value1", "skill2": "value2"},
+    "Status": "Active",
+}
 DOCENTEN = ["Jeroen", "Robert"]
 STUDENTEN = ["Roy", "Kyan", "Jasper", "Sebastiaan"]
-
-
-import psycopg2
-from psycopg2 import Error
 
 
 # Function to establish a connection to PostgreSQL database
@@ -43,42 +51,73 @@ def populate_database(connection):
         # Start transaction
         connection.autocommit = False
         cursor = connection.cursor()
-        #  Create a new organisation
+
+        # Create a new organisation
         cursor.execute(
             f"INSERT INTO Organisation (Name) VALUES ('{ORGANISATION}') RETURNING id"
         )
         organisation_id = cursor.fetchone()[0]
 
         # Create a team faculty
-
         cursor.execute(
             f"INSERT INTO TeamFaculty (Name, OrgansationId) VALUES ('{TEAMFACULTY}', {organisation_id}) RETURNING id"
         )
         faculty_id = cursor.fetchone()[0]
 
         # Create a team
-
         cursor.execute(
             f"INSERT INTO Teams (Name, FacultyId) VALUES ('{TEAM}', {faculty_id}) RETURNING id"
         )
         team_id = cursor.fetchone()[0]
 
-        # Create a education module and initial module version
+        # Create an education module and initial module version
+
+        cursor.execute(
+            f"INSERT INTO EducationModule (Name, TeamId) VALUES ('{COURSE}', {team_id}) RETURNING id"
+        )
+        module_id = cursor.fetchone()[0]
+
+        MODULE_VERSION_DATA["EducationModuleId"] = module_id
+
+        cursor.execute(
+            f"""INSERT INTO EducationModuleVersion (Version, Description, Attributes, RequiredAchievements, Skills, EducationModuleID, Status) 
+            VALUES ('{MODULE_VERSION_DATA['Version']}', '{MODULE_VERSION_DATA['Description']}', '{json.dumps(MODULE_VERSION_DATA['Attributes'])}', 
+            '{json.dumps(MODULE_VERSION_DATA['RequiredAchievements'])}', '{json.dumps(MODULE_VERSION_DATA['Skills'])}', 
+            '{MODULE_VERSION_DATA['EducationModuleId']}', '{MODULE_VERSION_DATA['Status']}')"""
+        )
+
+        # Onboarding issuer
+        onboard_issuer_input = {
+            "issuanceKeyConfig": {"type": "jwk", "algorithm": "secp256r1"},
+            "issuerDidConfig": {"method": "jwk"},
+        }
 
         # Create the users for the team
         for docent in DOCENTEN:
-            # get DID and JWK from the issuer api for the docent
-            response = requests.get(f"https://api.issuer.open-ict.nl/issuer/{docent}")
+            # Get DID and JWK from the issuer API for the docent
+            response = requests.post(
+                url="http://localhost:7002/onboard/issuer",
+                json=onboard_issuer_input,
+                timeout=5,
+            )
+            did = response.json()["issuerDid"]
+            issuance_key = response.json()["issuanceKey"]
+            issuance_key["jwk"] = json.dumps(issuance_key["jwk"])
+
             cursor.execute(
-                f"INSERT INTO Users (Name, Did, Jwk) VALUES ('{docent}', 'Docent', {team_id})"
+                f"INSERT INTO Users (Name, Did, IssuanceKey) VALUES ('{docent}', '{did}', '{json.dumps(issuance_key)}') RETURNING id"
+            )
+
+            user_id = cursor.fetchone()[0]
+
+            # Add user to the team
+            cursor.execute(
+                f"INSERT INTO TeamUsers (TeamId, UserId) VALUES ({team_id}, {user_id})"
             )
 
         # Create users for the students
-
         for student in STUDENTEN:
-            cursor.execute(
-                f"INSERT INTO Users (Name, Did, Jwk) VALUES ('{student}', 'Student', {team_id})"
-            )
+            cursor.execute(f"INSERT INTO Users (Name) VALUES ('{student}')")
 
         # Commit transaction if all queries succeed
         connection.commit()
