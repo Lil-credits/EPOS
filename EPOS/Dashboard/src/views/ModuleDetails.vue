59592f563d560f1243d90c566f87cc47bc070d1c
Kyanvzijp <kyan.vanzijp@student.hu.nl>
2024-05-15 10:55:40 +0200

    Update ModuleDetails.vue

---
+++ EPOS/Dashboard/src/views/ModuleDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <!-- Your HTML content goes here -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ModuleDetails',
+    // Your component's logic goes here
+}
+</script>
+
+<style scoped>
+/* Your component's styles go here */
+</style>
