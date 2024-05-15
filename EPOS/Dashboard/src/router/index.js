c02e24be87bd4011732f22410bd2d16dc4d5b1a3
echo "Jasper <jasper@veraart.cloud>
2024-05-15 10:35:55 +0200

    Create router setup

--- EPOS/Dashboard/src/router/index.js
+++ EPOS/Dashboard/src/router/index.js
@@ -3,10 +3,21 @@ import Form from '../views/FormPage.vue';
 
 const routes = [
   {
-    path: '/', // Corrected and changed to kebab-case
-    name: 'FormPage',
+    path: '/modules/',
+    name: 'ModuleOveriew',
+    component: ModuleOveriew
+  },
+  {
+    path: '/modules/create', // Corrected and changed to kebab-case
+    name: 'CreateModule',
     component: Form,
-}
+  },
+  {
+    path: '/modules/:id',
+    name: 'ModuleDetails',
+    component: ModuleDetails,
+    props: true // This allows route params to be passed as props to the component
+  }
 ];
 
 const router = createRouter({
