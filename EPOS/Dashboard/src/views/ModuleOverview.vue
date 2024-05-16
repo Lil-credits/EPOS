8f6eb80b94fc102fe186af90635cff574ebdb7dc
echo "Jasper <jasper@veraart.cloud>
2024-05-16 16:24:53 +0200

    Update ModuleOverview.vue

--- EPOS/Dashboard/src/views/ModuleOverview.vue
+++ EPOS/Dashboard/src/views/ModuleOverview.vue
@@ -1,41 +1,52 @@
 <template>
   <div>
-      <v-icon class="back-arrow" @click="goBack">mdi-arrow-left</v-icon>
-      <div class="overview">
-          <div class="circle-container">
-          <v-btn class="circle-button module" fab small @click="$router.push('modules/1')">
-          <img src="../assets/Picture1.png" class="circle-image" alt="Sales Engineer">
-          </v-btn>
-          <div class="circle-text">Sales Engineer</div>
-          </div>
-          <div class="circle-container">
-          <v-btn class="circle-button module" fab small @click="$router.push('modules/2')">
-          <img src="../assets/Picture15.png" class="circle-image" alt="Process Engineer">
-          </v-btn>
-          <div class="circle-text">Process Engineer</div>
-          </div>
-          <div class="circle-container">
-          <v-btn class="circle-button create-own" fab small @click="$router.push('modules/create')">
-          <v-icon>mdi-plus</v-icon>
-          </v-btn>
-          <div class="circle-text">Create your own</div>
-          </div>
-      </div>
-      </div>
+    <v-icon class="back-arrow" @click="goBack">mdi-arrow-left</v-icon>
 
-      <CircleModule/>
+    <div class="overview">
+      <!-- Corrected attribute spacing and removed unnecessary comma -->
+      <CircleModule title="Sales Engineer" image="" moduleId="4" />
+
+      <div class="circle-container">
+        <v-btn class="circle-button create-own" fab small @click="$router.push('/modules/create')">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+        <div class="circle-text">Create your own</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import CircleModule from "../components/module-components/CircleModule.vue";
+import axios from 'axios';
+import CircleModule from '../components/module-components/CircleModule.vue';
+import { onMounted } from 'vue';  // Corrected import
 
 export default {
+<<<<<<< Updated upstream
   name: 'CircleModule',
     components: {
         CircleModule
     },
 
   
+=======
+  name: 'ModuleOverview',  // Corrected component name
+  components: {
+    CircleModule
+  },
+  setup() {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/v1/education-modules");
+        console.log(response);
+      } catch {
+        console.log("Can't get data");
+      }
+    };
+
+    onMounted(fetchData);
+  }
+>>>>>>> Stashed changes
 };
 </script>
 
@@ -73,7 +84,6 @@ export default {
   background-color: #FB0017;
   color: Black;
 }
-
 .create-own {
   background-color: #00A4E7;
   color: white;
