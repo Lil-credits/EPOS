40cf2157eae5315d79b20079cc08ae2ee2e0cf2e
echo "Jasper <jasper@veraart.cloud>
2024-05-30 11:13:36 +0200

    Create first version for page heading

--- EPOS/Dashboard/src/App.vue
+++ EPOS/Dashboard/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app>
     <v-main>
+      <pageHeading/>
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import pageHeading from '@/components/pageHeading.vue';
 export default {
   name: 'App',
-  // No data is needed if you don't have global states here
+  components: {
+    pageHeading
+  }
 };
 </script>
---
+++ EPOS/Dashboard/src/components/pageHeading.vue
@@ -0,0 +1,63 @@
+<template>
+<v-container class="page-header">
+            <v-row align="center" justify="space-between">
+                <v-col cols="auto">
+                    <v-icon class="backButton" @click="goBack">mdi-arrow-left</v-icon>
+          
+        </v-col>
+        <v-col cols="auto">
+            <v-breadcrumbs class="title"
+                :items="[{
+                    text: 'HU',
+                    disabled: false,
+                    href: '/modules'
+                }, {
+                    text: title,
+                    disabled: true
+                }]"
+                divider="/"/>
+        </v-col>
+        <v-col cols="auto">
+          <v-avatar size="40">
+            <img src="../assets/profile.jpg" alt="User Avatar">
+          </v-avatar>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PageHeader',
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+    methods: {
+      goBack() {
+        this.$router.go(-1);
+      },
+    },
+  };
+  </script>
+  
+<style scoped>
+
+  .page-header {
+    background-color: #f2f2f2;
+    color: #ffffff;
+    padding: 10px;
+    margin-bottom: 20px;
+  }
+  .title {
+    color: ;
+    text-align: center;
+  }
+    .backButton {
+        cursor: pointer;
+        color: black;
+    }
+  </style>
+  
