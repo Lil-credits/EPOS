dc472085fbb17b9594c881700350c37fc7faea22
echo "Jasper <jasper@veraart.cloud>
2024-05-30 10:36:54 +0200

    Added createoverview

---
+++ EPOS/Dashboard/src/components/page-components/module/form/createOverview.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!-- Your component's HTML template goes here -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'YourComponentName',
+    data() {
+        return {
+            // Your component's data goes here
+        };
+    },
+    methods: {
+        // Your component's methods go here
+    },
+    mounted() {
+        // Code to run when the component is mounted goes here
+    },
+};
+</script>
+
+<style scoped>
+/* Your component's styles go here */
+</style>
