72a2d99d0ea75faf52f4589e88f80c0a563afe57
echo "Jasper <jasper@veraart.cloud>
2024-06-06 12:50:03 +0200

    WIP: Breadcrumb

--- EPOS/Dashboard/src/components/pageHeading.vue
+++ EPOS/Dashboard/src/components/pageHeading.vue
@@ -1,62 +1,54 @@
 <template>
-<v-container class="page-header">
-            <v-row align="center" justify="space-between">
-                <v-col cols="auto">
-                    <v-icon class="backButton" @click="goBack">mdi-arrow-left</v-icon>
-          
-        </v-col>
-        <v-col cols="auto">
-            <v-breadcrumbs class="title"
-                :items="[{
-                    text: 'HU',
-                    disabled: false,
-                    href: '/modules'
-                }, {
-                    text: title,
-                    disabled: true
-                }]"
-                divider="/"/>
-        </v-col>
-        <v-col cols="auto">
-          <v-avatar size="40">
-                <img :src="require('../assets/profile.jpg')" alt="User Avatar" style="max-width: 100%; max-height: 100%;" @click="$router.push('/')">
-            </v-avatar>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    name: 'PageHeader',
-    props: {
-      title: {
-        type: String,
-        required: true,
-      },
-    },
-    methods: {
-      goBack() {
-        this.$router.go(-1);
-      },
-    },
-  };
-  </script>
-  
-<style scoped>
+  <v-container class="page-header">
+    <v-row align="center" justify="space-between">
+      <v-col cols="auto">
+        <v-icon class="backButton" @click="goBack">mdi-arrow-left</v-icon>
+      </v-col>
+      <v-col cols="auto">
+        <v-breadcrumbs class="title"
+                      :items="[{
+                        text: 'Dashboard',
+                        disabled: false,
+                        href: '/',
+                      }, {
+                        text: title,
+                        disabled: true,
+                      }]"
+                      divider="/"/>
+      </v-col>
+      <v-col cols="auto">
+        <v-avatar size="40">
+          <img :src="require('../assets/profile.jpg')" alt="User Avatar" style="max-width: 100%; max-height: 100%;" @click="$router.push('/')">
+        </v-avatar>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
-  .page-header {
-    background-color: white;
-    color: #ffffff;
-    padding: 10px;
-  }
-  .title {
-    color: white;
-    text-align: center;
-  }
-    .backButton {
-        cursor: pointer;
-        color: black;
-    }
-  </style>
-  
+<script>
+export default {
+  name: 'PageHeader',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.page-header {
+  padding: 10px;
+}
+
+.backButton {
+  cursor: pointer;
+  color: black;
+}
+</style>
