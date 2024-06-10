2a5c5f9bc1c27ba508a2ae43960cc242e475ca3c
Kyanvzijp <kyan.vanzijp@student.hu.nl>
2024-06-10 14:54:56 +0200

    ..

--- EPOS/Dashboard/src/plugins/vuetify.js
+++ EPOS/Dashboard/src/plugins/vuetify.js
@@ -1,10 +1,21 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import colors from 'vuetify/util/colors'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: colors.red.darken1, // #007bff
+          secondary: colors.red.lighten4, // #FFCDD2
+        }
+      },
+    },
+  },
+})
